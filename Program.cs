dc4878b3f6c22e838835b7e00c7b74bc4b432e18
Frederik <git@caf-tranberg.dk>
2022-03-07 21:22:32 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CS_cfgconfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getcsgodir getcsgodir = new getcsgodir();
+            string csgodir = getcsgodir.GetCSGODir();
+            if (csgodir == null)
+            {
+                Console.WriteLine("CS:GO is not installed");
+                Console.ReadKey();
+                System.Environment.Exit(1);
+            }
+            Console.WriteLine("Program is copyrighted by Head9x - head9x.dk");
+            copydir copydir = new copydir();
+
+            copydir.CopyDirectory(@".\cfg", csgodir+"\\cfg", true);
+            copydir.CopyDirectory(@".\resource", csgodir + "\\resource", true);
+            Console.WriteLine("!!Remember to add the following line to launch options to activate TaeCFG!!");
+            Console.WriteLine("");
+            Console.WriteLine("+exec taeexec.cfg -language taeware -tickrate 128 -d3d9ex +mat_disable_fancy_blending 1 -high -r_emulate_g -softparticlesdefaultoff -no-browser -console -novid -nojoy -limitvsconst -forcenovsync +mat_queue_mode 2");
+            Console.WriteLine("");
+            Console.ReadKey();
+
+            //Snippets were grabbed from other sources. Linked below:
+            //https://gist.github.com/moritzuehling/7f1c512871e193c0222f#file-getcsgodir-cs
+        }
+    }
+}
---
+++ getcsgodir.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CS_cfgconfig
+{
+    class getcsgodir
+    {
+        public string GetCSGODir()
+        {
+            string steamPath = (string)Registry.GetValue("HKEY_CURRENT_USER\\Software\\Valve\\Steam", "SteamPath", "");
+            string pathsFile = Path.Combine(steamPath, "steamapps", "libraryfolders.vdf");
+
+            if (!File.Exists(pathsFile))
+                return null;
+
+            List<string> libraries = new List<string>();
+            libraries.Add(Path.Combine(steamPath));
+
+            var pathVDF = File.ReadAllLines(pathsFile);
+            Regex pathRegex = new Regex(@"\""(([^\""]*):\\([^\""]*))\""");
+            foreach (var line in pathVDF)
+            {
+                if (pathRegex.IsMatch(line))
+                {
+                    string match = pathRegex.Matches(line)[0].Groups[1].Value;
+                    libraries.Add(match.Replace("\\\\", "\\"));
+                }
+            }
+
+            foreach (var library in libraries)
+            {
+                string csgoPath = Path.Combine(library, "steamapps\\common\\Counter-Strike Global Offensive\\csgo");
+                if (Directory.Exists(csgoPath))
+                {
+                    return csgoPath;
+                }
+            }
+
+            return null;
+        }
+    }
+}
